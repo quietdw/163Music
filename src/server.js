e4b1cdb056ee2ffca78dad0bf2ca34f24f182ea4
quietdw <1044569102@qq.com>
2019-01-21 23:02:47 +0800

    生成uptoken

---
+++ src/server.js
@@ -0,0 +1,57 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+var qiniu = require('qiniu')
+
+if (!port) {
+    console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+    process.exit(1)
+}
+
+var server = http.createServer(function (request, response) {
+    var parsedUrl = url.parse(request.url, true)
+    var path = request.url
+    var query = ''
+    if (path.indexOf('?') >= 0) {
+        query = path.substring(path.indexOf('?'))
+    }
+    var pathNoQuery = parsedUrl.pathname
+    var queryObject = parsedUrl.query
+    var method = request.method
+
+    /******** 从这里开始看，上面不要看 ************/
+    if (path == '/uptoken') {
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/json;charset=utf-8')
+        response.setHeader('Access-Control-Allow-Origin', '*')
+
+        var config = JSON.parse(fs.readFileSync("./qiniu-key.json"))
+        
+        let {accessKey, secretKey} = config
+
+        var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+
+        var options = {
+            scope: '163music',
+          };
+          var putPolicy = new qiniu.rs.PutPolicy(options);
+          var uploadToken=putPolicy.uploadToken(mac);
+
+        response.write(`
+            {
+                "uptoken":"${uploadToken}"
+            }
+        `)
+        response.end()
+    } else {
+        response.statusCode = 404
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.end()
+    }
+    /******** 代码结束，下面不要看 ************/
+
+})
+
+server.listen(port)
+console.log('服务已启动\n打开 http://127.0.0.1:'+port+'/')

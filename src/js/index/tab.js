90fa4ec872d58c07b05dd83616679cd29687cb61
quietdw <1044569102@qq.com>
2019-02-16 22:00:55 +0800

    tabs模块化

---
+++ src/js/index/tab.js
@@ -0,0 +1,22 @@
+{
+    let view = {
+        el:'#tabs'
+    }
+    let model = {}
+    let controller = {
+        init(view,model){
+            this.view = view
+            this.model = model
+            this.bindEvents()
+        },
+        bindEvents(){
+            $(this.view.el).on('click','.tabs-nav>li', (e)=>{
+                let oLi = $(e.currentTarget)
+                oLi.addClass('active').siblings().removeClass('active')
+                tabName = oLi.attr('data-tab-name')
+                window.eventHub.emit('selectTab',tabName)
+            })
+        }
+    }
+    controller.init.call(controller,view,model)
+}

be48d04626b89f3218ef314800ecee36ec89a21c
quietdw <1044569102@qq.com>
2019-02-16 23:34:18 +0800

    获取最新歌曲并渲染

---
+++ src/js/index/newestSongs.js
@@ -0,0 +1,59 @@
+{
+    let view = {
+        el: 'section.songs',
+        render(data) {
+            let {songs} = data
+            songs.map((song) => {
+                let oLi = $(`
+            <li>
+            <h3>${song.name}</h3>
+            <p>
+              <svg class="icon icon-sq">
+                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-sq"></use>
+              </svg>
+              ${song.singer}
+            </p>
+            <a class="playButton" href="${song.url}">
+              <svg class="icon icon-play">
+                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-play"></use>
+              </svg>
+            </a>
+          </li>
+            `)
+
+            $('section.songs>#songs').append(oLi)
+            })
+
+
+
+        }
+    }
+    let model = {
+        data: {
+            songs:[]
+        },
+        fetch() {
+            var query = new AV.Query('Song');
+            return query.find().then((songs) => {
+                this.data.songs = songs.map((song) => {
+                    return {
+                        id: song.id,
+                        ...song.attributes
+                    }
+                });
+                return songs
+            });
+        }
+    }
+    let controller = {
+        init(view, model) {
+            this.view = view
+            this.model = model
+            this.model.fetch().then(() => {
+                this.view.render(this.model.data)
+            })
+        }
+    }
+
+    controller.init.call(controller, view, model)
+}
